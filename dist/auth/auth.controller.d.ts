e57e309454be5f0106dac35f9c708ff3c38dd0f3
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-09-10 22:15:58 +0700

    Try prisma orm

---
+++ dist/auth/auth.controller.d.ts
@@ -0,0 +1,18 @@
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+export declare class AuthController {
+    private authService;
+    constructor(authService: AuthService);
+    signup(dto: AuthDto): Promise<{
+        id: number;
+        createdAt: Date;
+        updatedAt: Date;
+        email: string;
+        hash: string;
+        firstName: string;
+        lastName: string;
+    }>;
+    signin(): {
+        msg: string;
+    };
+}
